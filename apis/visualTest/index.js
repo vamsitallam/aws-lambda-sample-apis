52cceb6aee342339407d3f538a6c9f741f1933ed
Vamsi Tallam <vamsitallam@ibhubs.co>
2022-05-11 15:27:10 +0530

    basic version of lamda is added

--- apis/visualTest/index.js
+++ apis/visualTest/index.js
@@ -1,55 +1,102 @@
 var http = require("http");
 const compareImages = require("resemblejs/compareImages");
 const fs = require("mz/fs");
-const puppeteer = require("puppeteer"); // Require Puppeteer module
+// const puppeteer = require("puppeteer-serverless");
+const chromium = require("chrome-aws-lambda");
 
 const fixtureData = require("./fixtures.json");
 
-// module.exports.visualTestDifference = async (event) => {
-//   if (event.httpMethod == "GET") {
-//   }
-// };
-const addCSStoHTML =
-  "<head>\n" + " " + "<style>\n" + fixtureData.css_code + "\n</style>";
+module.exports.visualTestDifference = async (event, context, callback) => {
+  if (event.httpMethod == "GET") {
+    const addCSStoHTML =
+      "<head>\n" + " " + "<style>\n" + fixtureData.css_code + "\n</style>";
 
-const appendCSSCodeToHTML = fixtureData.html_code.replace(
-  "<head>\n",
-  addCSStoHTML
-);
-const addJStoHTMLAndCSS =
-  "<script>\n" + fixtureData.js_code + "\n</script>" + "\n</body>";
+    const appendCSSCodeToHTML = fixtureData.html_code.replace(
+      "<head>\n",
+      addCSStoHTML
+    );
+    const addJStoHTMLAndCSS =
+      "<script>\n" + fixtureData.js_code + "\n</script>" + "\n</body>";
 
-const appendJSCodeToHTMLAndCSS = appendCSSCodeToHTML.replace(
-  "\n</body>",
-  addJStoHTMLAndCSS
-);
+    const appendJSCodeToHTMLAndCSS = appendCSSCodeToHTML.replace(
+      "\n</body>",
+      addJStoHTMLAndCSS
+    );
 
-var server = http.createServer(function (req, res) {
-  res.setHeader("Content-Type", "text/html");
-  res.writeHead(200);
-  res.end(appendJSCodeToHTMLAndCSS);
-});
+    var server = http.createServer(function (req, res) {
+      res.setHeader("Content-Type", "text/html");
+      res.writeHead(200);
+      res.end(appendJSCodeToHTMLAndCSS);
+    });
 
-server.listen(5000); //3 - listen for any incoming requests
+    server.listen(5000); //3 - listen for any incoming requests
 
-console.log("Node.js web server at port 5000 is running..");
+    console.log("Node.js web server at port 5000 is running..");
 
-const screenShot = async (url, path) => {
+    const result = await generateScreenshotAndCalculateDiff(callback);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(result),
+    };
+  }
+};
+
+const screenShot = async (url, path, callback) => {
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto(url);
+    await page.screenshot({
+      path: path, // Save the screenshot in current directory
+      fullPage: true, // take a fullpage screenshot
+    });
+
+    result = await page.title();
+  } catch (error) {
+    return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  return callback(null, result);
+
   // Define Screenshot function
 
-  const browser = await puppeteer.launch(); // Launch a "browser"
+  // console.log(puppeteer.default.launch);
 
-  const page = await browser.newPage(); // Open a new page
+  // const browser = await puppeteer.default.launch({
+  //   executablePath: "/home/ib_admin/.config/google-chrome",
+  // });
+  // const page = await browser.newPage();
 
-  await page.goto(url); // Go to the website
+  // const page = await browser.newPage(); // Open a new page
 
-  await page.screenshot({
-    path: path, // Save the screenshot in current directory
-    fullPage: true, // take a fullpage screenshot
-  });
+  // await page.goto(url); // Go to the website
 
-  await page.close(); // Close the website
-  await browser.close(); // Close the browser
+  // await page.screenshot({
+  //   path: path, // Save the screenshot in current directory
+  //   fullPage: true, // take a fullpage screenshot
+  // });
+
+  // await page.close(); // Close the website
+  // await browser.close(); // Close the browser
 };
 
 async function getDiff(expectedPath, actualPath) {
@@ -78,21 +125,21 @@ async function getDiff(expectedPath, actualPath) {
   return data.misMatchPercentage;
 }
 
-var generateScreenshotAndCalculateDiff = async () => {
+var generateScreenshotAndCalculateDiff = async (callback) => {
   const url = "http://localhost:5000";
 
-  const path = "./actual/screenshot.png";
+  const path = "tmp/actual/screenshot.png";
 
-  await screenShot(url, path);
-  const expectedPath = `./expected/screenshot.png`;
-  const actualPath = `./actual/screenshot.png`;
+  await screenShot(url, path, callback);
+  const expectedPath = `tmp/expected/screenshot.png`;
+  const actualPath = `tmp/actual/screenshot.png`;
   const misMatchPercentage = await getDiff(expectedPath, actualPath);
   const matchPercentage = 100 - parseFloat(misMatchPercentage);
   console.log(
     "🚀 ~ file: index.js ~ line 91 ~ generateScreenshotAndCalculateDiff ~ matchPercentage",
     matchPercentage
   );
-  process.exit();
+  return matchPercentage;
+  // return 100;
+  // process.exit();
 };
-
-generateScreenshotAndCalculateDiff();
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const slsw = require("serverless-webpack");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: "node",
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: [/\.tsx?$/],
+        exclude: [/node_modules/, /\.test.tsx?$/],
+        use: [{}],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      STAGE: `"${slsw.lib.options.stage}"`,
+    }),
+  ],
+  resolve: {
+    modules: ["node_modules", "scripts"],
+    extensions: [".ts", ".tsx", ".json", ".js", ".jsx"],
+  },
+};
